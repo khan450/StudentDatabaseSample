2005430e60cc531f4f5fef63a772bc40e3b2bce4
khan450 <khangeorgem@gmail.com>
2024-10-30 08:39:08 -0400

    Initial commit

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace StudentDatabaseSample.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<StudentDatabaseSample.SchoolContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "StudentDatabaseSample.SchoolContext";
+        }
+
+        protected override void Seed(StudentDatabaseSample.SchoolContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabaseSample
+{
+    // Student class
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    // SchoolContext class for Entity Framework
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+
+    // Main Program class
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SchoolContext())
+            {
+                // Prompt to enter a student's name
+                Console.Write("Enter the name of a new Student: ");
+                var name = Console.ReadLine();
+
+                // Create and add a new Student
+                var student = new Student { Name = name, Age = 20 };
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                // Retrieve and display all students
+                var query = from s in db.Students
+                            orderby s.Name
+                            select s;
+
+                Console.WriteLine("All students in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine($"ID: {item.StudentId}, Name: {item.Name}, Age: {item.Age}");
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
